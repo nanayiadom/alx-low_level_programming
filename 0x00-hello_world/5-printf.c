590226f638124a5720d1182037955c4db5d3541d
nanayiadom <fybooakye99@gmail.com>
2023-06-16 19:23:13 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,6 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{puts("Programming is like building a multilingual puzzle");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
