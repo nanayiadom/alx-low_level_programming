4da43bdb1bd79c1023c1c00348e7128ddb622379
nanayiadom <fybooakye99@gmail.com>
2023-06-16 17:32:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-/*Main is a bitch*/
-{printf("with proper grammar, but the outcome is a piece of art,\n");
+{puts(“Programming is like building a multilingual puzzle”);
 return (0);
 }
