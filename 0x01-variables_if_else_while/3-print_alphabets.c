0e249de6ec97c999fe51c090d62df5840b9927a2
nanayiadom <fybooakye99@gmail.com>
2023-06-19 04:45:57 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,18 @@
 #include<stdio.h>
 /**
- *main - Print alphabet in lowercase and uppercase
+ *main - Print alphabets
  *Return : always 0
- *aph - defines alphabet
+ *aph - alphabet
  */
 int main(void)
 {
-	char aph([52])= "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	char aph([52]) = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	int i;
 
-	for (i=0; i < 52; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(aph[i]);
 
 	}
 		putchar("\n");
-	
 	return (0);
