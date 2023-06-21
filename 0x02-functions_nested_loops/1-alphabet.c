175fffc6f17e87d51b8d6d5a3edc160e85516be3
nanayiadom <fybooakye99@gmail.com>
2023-06-21 11:23:59 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: Always return 0
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar('c');
+	}
+	_putchar("\n");
+
+	return (0);
+}
