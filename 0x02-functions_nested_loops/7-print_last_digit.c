f0cb6d37e2e045441491e64b8392e20eb1535288
nanayiadom <fybooakye99@gmail.com>
2023-06-21 15:01:59 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int a;
+	int r;
 
 	print_last_digit(98);
 	print_last_digit(0);
-	a = print_last_digit(-1024);
-	_putchar('0' + a);
+	r = print_last_digit(-1024);
+	_putchar('0' + r);
 	_putchar('\n');
 	return (0);
 }
