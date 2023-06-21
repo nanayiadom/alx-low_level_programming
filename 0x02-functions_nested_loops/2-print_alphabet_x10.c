b4721e734c9a006236459e2890be4d46ec36a266
nanayiadom <fybooakye99@gmail.com>
2023-06-21 13:21:26 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int 10x_alphabet = 10;
+
+}
+
+void print_alphabet_x10(void);
+{
+	char alphabets;
+
+	for (10x_alphabet = 0; 10x_alphabet < 10; 10x_alphabet = 10x_alphabet + 1)
+	{
+		for (alphabets = 'a'; alphabets <= 'z'; alphabets = alphabets + 1)
+		{
+			_putchar('aphabets');
+			_putchar('\n');
+		}
+
+	}
+		_putchar("%d", 10x_alphabet);
+		_putchar('\n');
+
+	return (0);
+}
