b7f4061928df7a671dd2d3e424c2d6c3bcd5bd98
nanayiadom <fybooakye99@gmail.com>
2023-06-21 14:55:00 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	print_last_digit(98);
+	print_last_digit(0);
+	r = print_last_digit(-1024);
+	_putchar('0' + a);
+	_putchar('\n');
+	return (0);
+}
