e6c4cdd09689079336c16f29c33115b9205f17ab
nanayiadom <fybooakye99@gmail.com>
2023-06-23 10:28:09 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - prints all numbers
+ *
+ * return:always 0
+ */
+
+void print_numbers(void);
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+
+	return (0);
+}
