c42dc63d8397d52d71484deb4ff6abe104754119
nanayiadom <fybooakye99@gmail.com>
2023-06-23 12:46:58 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,15 @@ void print_diagonal(int n)
 		_putchar('\n');
 	} else
 	{
-		int a, b;
+		int i, j;
 
-		for (a = 0; a < n; a++)
+		for (i = 0; i < n; i++)
 		{
-			for (b = 0; b < n; b++)
+			for (j = 0; j < n; j++)
 			{
-				if (b == i)
+				if (j == i)
 					_putchar('\\');
-				else if (b < a)
+				else if (j < i)
 					_putchar(' ');
 			}
 			_putchar('\n');
